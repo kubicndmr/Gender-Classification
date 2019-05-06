d61d74c2ba0f22a4ece478a6ecc4d90fc89dcf38
kubicndmr <44262506+kubicndmr@users.noreply.github.com>
2019-05-06 23:58:24 +0200

    model upgraded
    script revisited from the top, validation set is added, test and validation sets are windowed with seed, preprocessing is embedded, instead of linear layers convolutional layers are used, printing choices are updated

--- main.py
+++ main.py
@@ -1,37 +1,48 @@
 import os
 import csv
+import copy
 import librosa
 import torch 
 import utils
+import time
+import pre_processing as pp 
 import numpy as np
-from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
 from scipy.fftpack import fft
 
-DATA_PATH = 'dataset/'
-TEST_PATH = 'testset/'
+PP = False #switch for using pre_processing.py
 
+valid = 10 # Percentage of the database to use for training and validation 
+test = 10
+sampling_rate = 16000
 inputDim = 256
-hiddenDim = 512
+hiddenDim = 1024
 classes = 1
+
+if PP:
+    pp.fileOrder(valid,test)
+    pp.resampleDatabase(sampling_rate)
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 # Process Dataset
-dataList,dataLabel = list(),list()
+trainList,trainLabel = list(),list()
+validList,validLabel = list(),list()
 testList,testLabel = list(),list()
 
-utils.generate_labels(DATA_PATH)
-utils.generate_labels(TEST_PATH)
+utils.generate_labels(pp.TRAIN_PATH)
+utils.generate_labels(pp.VALID_PATH)
+utils.generate_labels(pp.TEST_PATH)
 
-dataList,dataLabel = utils.load_labels(DATA_PATH)
-testList,testLabel = utils.load_labels(TEST_PATH)
+trainList,trainLabel = utils.load_labels(pp.TRAIN_PATH)
+validList,validLabel = utils.load_labels(pp.VALID_PATH)
+testList,testLabel = utils.load_labels(pp.TEST_PATH)
 
-
-class SpeechDataset(Dataset):
+class TrainingDataset(Dataset):
     
     def __init__(self,dataList,dataLabel,path,inputDim):
         self.dataList = dataList
@@ -48,92 +59,154 @@ class SpeechDataset(Dataset):
     
     def __getitem__(self,idx):
         wav,sr = librosa.load(self.path+'/'+self.dataList[idx],sr=16000,duration=2.0)
-        wav = SpeechDataset.window(self,wav,sr)
-        f = np.log(np.abs(fft(wav)))
+        wav = TrainingDataset.window(self,wav,sr)
+        f = np.abs(fft(wav))
         if self.dataLabel[idx]=='f':
             l = np.asarray([0]).astype(float)
         elif self.dataLabel[idx]=='m':
             l = np.asarray([1]).astype(float)
         return [f,l]
+
+
+trainSet = TrainingDataset(trainList,trainLabel,pp.TRAIN_PATH,inputDim)
+
+
+class TestingDataset(Dataset):
     
+    def __init__(self,dataList,dataLabel,path,inputDim,randomArray,sampling_rate):
+        self.dataList = dataList
+        self.dataLabel = dataLabel
+        self.path = path
+        self.inputDim = inputDim
+        self.randomArray = randomArray
+        self.sampling_rate = sampling_rate
+        
+    def __len__(self):
+        return len(self.dataList)
+    
+    def window(self,x,idx):
+        offset = self.randomArray[idx]
+        return x[offset:offset+self.inputDim]
+    
+    def __getitem__(self,idx):
+        wav,sr = librosa.load(self.path+'/'+self.dataList[idx],sr=16000,duration=2.0)
+        wav = TestingDataset.window(self,wav,idx)
+        f = np.abs(fft(wav))
+        if self.dataLabel[idx]=='f':
+            l = np.asarray([0]).astype(float)
+        elif self.dataLabel[idx]=='m':
+            l = np.asarray([1]).astype(float)
+        return [f,l]
 
-trainSet = SpeechDataset(dataList,dataLabel,DATA_PATH,inputDim)
-testSet = SpeechDataset(testList,testLabel,TEST_PATH,inputDim)
 
+np.random.seed(101)
+randomArrayVal = np.random.randint(0,sampling_rate-inputDim-1,size=len(validList))
+np.random.seed(102)
+randomArrayTe = np.random.randint(0,sampling_rate-inputDim-1,size=len(testList))
 
+validSet = TestingDataset(validList,validLabel,pp.VALID_PATH,inputDim,randomArrayVal,sampling_rate)
+testSet = TestingDataset(testList,testLabel,pp.TEST_PATH,inputDim,randomArrayTe,sampling_rate)
 
 trainLoader = torch.utils.data.DataLoader(dataset=trainSet,batch_size=4,shuffle=True)
-testLoader = torch.utils.data.DataLoader(dataset=testSet,batch_size=8,shuffle=True)
+validLoader = torch.utils.data.DataLoader(dataset=validSet,batch_size=4,shuffle=False)
+testLoader = torch.utils.data.DataLoader(dataset=testSet,batch_size=8,shuffle=False)
 
 
 class Model(nn.Module):
     def __init__(self,in_dim,hidden_dim,num_classes):
         super(Model, self).__init__()
-        self.layer1 = nn.Linear(in_dim,hidden_dim)
-        self.layer2 = nn.Linear(hidden_dim,hidden_dim)
+        self.layer1 = nn.Conv1d(in_dim,hidden_dim,kernel_size=11,padding=5)
+        self.layer2 = nn.Conv1d(hidden_dim,hidden_dim,kernel_size=11,padding=5)
         self.layer3 = nn.Linear(hidden_dim,num_classes)
+        self.activation = nn.Softmax(dim=0)
         
     def forward(self,x):
+        x = x.unsqueeze(-1)
         a = self.layer1(x)
+        a = self.activation(a)
         a = self.layer2(a)
+        a = self.activation(a)
+        a = a.squeeze()
         a = self.layer3(a)
-        return torch.sigmoid(a)
-    
+        return a
 
 model = Model(inputDim,hiddenDim,classes)
 
 
-def trainModel(model,numIter,loader,learning_rate):
-    cost = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
-    loss = [None]*numIter
+def trainModel(model,num_epochs,loaders,learning_rate):
     
-    for epoch in range(numIter):
-        average_cost = 0
-        for i, (fSpec,labels) in enumerate(loader):
-            f = torch.tensor(fSpec,dtype=torch.float,device=device)
-            y = torch.tensor(labels,dtype=torch.float,device=device)
+    criteria = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
+    trnLoss = [None]*num_epochs
+    valLoss = [None]*num_epochs
+    
+    for epoch in range(num_epochs):
+        
+        trn_loss = 0
+        corrects = 0
+        
+        for phase in ['train','valid']:
+            if phase == 'train':
+                model.train()
+            elif phase == 'valid':
+                model.eval()
+        
+            for i, (fSpec,labels) in enumerate(loaders[phase]):
+                f = fSpec.clone().detach().float()
+                y = labels.clone().detach().float()
             
-            y_hat = model(f)
-            currentCost = cost(y_hat,y)
+                optimizer.zero_grad()
+                
+                y_hat = model(f)
+                currentCost = criteria(y_hat,y)
             
-            optimizer.zero_grad()
-            currentCost.backward()
-            optimizer.step()
-            
-            average_cost += currentCost.item()
-            
-        average_cost = average_cost / len(dataLabel)    
-        loss[epoch] = average_cost
-        print('Epoch| '+ str(epoch+1))
-        print('Loss | '+ str(loss[epoch]))
+                if phase=='train':
+                    currentCost.backward()
+                    optimizer.step()
+                    trn_loss += currentCost.item()
+                
+                elif phase == 'valid':
+                    output = model(f)
+                    output[output>=0.5] = 1
+                    output[output<0.5] = 0
+                    corrects += torch.sum(output== y)
+                        
+        avg_trn_loss = trn_loss / len(trainLabel)
+        avg_val_loss = corrects.numpy() / len(validLabel)
+        trnLoss[epoch] = avg_trn_loss
+        valLoss[epoch] = avg_val_loss
+        print('Epoch          | '+ str(epoch+1))
+        print('Training Loss  | '+ str(avg_trn_loss))
+        print('Validation Loss| ' + str(avg_val_loss))
     
     fig = plt.figure()
-    plt.plot(range(numIter),loss)
+    plt.plot(range(num_epochs),trnLoss, 'b',label='Training Loss')
+    plt.plot(range(num_epochs),valLoss, 'r',label='Validation Loss')
+    plt.legend(loc=1)
     fig.suptitle('Gender Classification', fontsize=24)
     plt.xlabel('epochs', fontsize=14)
-    plt.ylabel('Loss (Cross Entropy)', fontsize=14)
+    plt.ylabel('Training Loss (MSE)', fontsize=14)
     plt.savefig('LossFunc.png')
 
-trainModel(model,150,trainLoader,0.0005)
+
+dataLoader = {'train':trainLoader,'valid':validLoader}
+
+trainModel(model=model,num_epochs=100,loaders=dataLoader,learning_rate = 1e-4)
         
 with torch.no_grad():
-    correct = 0
-    total = len(testLabel)
+    corrects = 0
+    
     for fSpec,labels in testLoader:
-        label = labels.to(device)
-        output = model(fSpec)
-        estimate = np.empty([1,len(label)])
-        for i in range(len(label)):
-            if output[i] >= 0.5:
-                estimate[0][i] = 1
-            elif output[i] < 0.5:
-                estimate[0][i] = 0
-            print('Ground Truth| '+str(label[i].item())+' Estimated| '+str(estimate[0][i]))
-                
-            correct += (estimate[0][i] == label[i].item())*1
+        f = fSpec.clone().detach().float()
+        y = labels.clone().detach().float()
+        
+        output = model(f)
+        output[output>=0.5] = 1
+        output[output<0.5] = 0
+        
+        corrects += torch.sum(output== y)
 
-    print('Accuracy of the network on the 23 test speech signals: {} %'.format(100 * correct / total))
+    print('Accuracy of the network on the 23 test speech signals: {}%'.format(100 * corrects / len(testList)))
 
 # Save the model checkpoint
 torch.save(model.state_dict(), 'model.ckpt')    
