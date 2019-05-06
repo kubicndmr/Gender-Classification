9d26349c5564d764fc93e2d469edcd6d4e17a4e1
kubicndmr <44262506+kubicndmr@users.noreply.github.com>
2019-05-06 23:59:32 +0200

    script updated

--- pre_processing.py
+++ pre_processing.py
@@ -1,21 +1,55 @@
 import os
 import librosa
+import shutil
+import random
 
 DATABASE_PATH = 'Lombard_Speech_Database_German/'
-TARGET_PATH = 'dataset/'
+TRAIN_PATH = 'dataset/'
+VALID_PATH = 'validset/'
+TEST_PATH = 'testset/'
 
-if not os.path.exists(TARGET_PATH):
-    os.makedirs(TARGET_PATH)
+def fileOrder(valid,test):
 
-#Copy all .wav files in subdirs to a single dir
-for dirs, subdirs, files in os.walk(DATABASE_PATH):
-    for name in files:
-        shutil.copy(dirs + '/' + name, TARGET_PATH)
+    if not os.path.exists(TRAIN_PATH):
+        os.makedirs(TRAIN_PATH)
+    
+    if not os.path.exists(VALID_PATH):
+        os.makedirs(VALID_PATH)
+        
+    if not os.path.exists(TEST_PATH):
+        os.makedirs(TEST_PATH)    
 
-#Resample to 16k
-for wav in os.listdir(TARGET_PATH):
-    resampled,_ = librosa.load(TARGET_PATH+wav,sr=16000,duration=2.0)
-    librosa.output.write_wav(TARGET_PATH+wav,resampled,16000)
+    temp_paths = list()
+    
+    #Copy all .wav files in subdirs to a single dir
+    for dirs, subdirs, files in os.walk(DATABASE_PATH):
+        for name in files:
+            shutil.copy(dirs + '/' + name, TRAIN_PATH)
+            temp_paths.append(name)
+    
+    numData = len(temp_paths)
+    numValid = numData // valid
+    numTest = numData // test
+    
+    randomSamples = random.sample(temp_paths,k=numValid+numTest) 
+    
+    for name in randomSamples[:numValid]:
+        shutil.move(TRAIN_PATH+name,VALID_PATH)
+        
+    for name in randomSamples[numTest:]:
+        shutil.move(TRAIN_PATH+name,TEST_PATH)        
+    
+    
+
+def resampleDatabase(sampling_rate):
+    
+    paths = [TRAIN_PATH,VALID_PATH,TEST_PATH]
+    
+    #Resample to 16k
+    for TARGET_PATH in paths:
+        for wav in os.listdir(TARGET_PATH):
+            resampled,_ = librosa.load(TARGET_PATH+wav,sr=sampling_rate)
+            librosa.output.write_wav(TARGET_PATH+wav,resampled,sampling_rate)
 
 
     
